21ea136d53ede3a7bb24d34f88d087789414aab5
Benzel <benawhiting@gmail.com>
2025-01-21 16:40:55 -0800

    Add starting prompt for game

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Please enter your name:");
+
+string name = Console.ReadLine();
+DateTime date = DateTime.UtcNow;
+
+Console.WriteLine("------------------------------------------");
+Console.WriteLine($"Hello {name}. It's {date}.");
+Console.WriteLine("Welcome to your math game.");
+Console.WriteLine(@"Please choose a game from the options below:
+A - Addition
+S - Subtraction
+M - Multiplication
+D - Division
+Q - Quit the program
+");
+Console.WriteLine("------------------------------------------");
+
+Console.ReadLine();
