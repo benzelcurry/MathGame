e45a04dfd2d17ae3174b5c575708d85602528265
Benzel <benawhiting@gmail.com>
2025-02-11 16:19:59 -0800

    Add branching

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -3,6 +3,9 @@
 string name = Console.ReadLine();
 DateTime date = DateTime.UtcNow;
 
+if (string.IsNullOrEmpty(name))
+    name = "Anonymous";
+
 Console.WriteLine("------------------------------------------");
 Console.WriteLine($"Hello {name}. It's {date.DayOfWeek}.");
 Console.WriteLine("Welcome to your math game.");
@@ -15,4 +18,25 @@ Q - Quit the program
 ");
 Console.WriteLine("------------------------------------------");
 
-Console.ReadLine();
+var gameSelected = Console.ReadLine().Trim().ToLower();
+
+if (gameSelected == "a")
+{
+    Console.WriteLine("Addition game selected.");
+}
+else if (gameSelected == "s")
+{
+    Console.WriteLine("Subtraction game selected.");
+}
+else if (gameSelected == "m")
+{
+    Console.WriteLine("Multiplication game selected.");
+}
+else if (gameSelected == "d")
+{
+    Console.WriteLine("Division game selected.");
+}
+else
+{
+    Console.WriteLine("Invalid input detected. Goodbye!");
+}
