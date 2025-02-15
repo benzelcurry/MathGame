f6bf404d4963213bc6cd99c64834d091d980c8da
Benzel <benawhiting@gmail.com>
2025-02-14 16:10:27 -0800

    Abstract menu logic to its own class

---
+++ MathGame/Menu.cs
@@ -0,0 +1,20 @@
+﻿namespace MathGame
+{
+    internal class Menu
+    {
+        static void Display(string name, DateTime date)
+        {
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine($"Hello {name}. It's {date.DayOfWeek}.");
+            Console.WriteLine("Welcome to your math game.");
+            Console.WriteLine(@"Please choose a game from the options below:
+                A - Addition
+                S - Subtraction
+                M - Multiplication
+                D - Division
+                Q - Quit the program
+                ");
+            Console.WriteLine("------------------------------------------");
+        }
+    }
+}
